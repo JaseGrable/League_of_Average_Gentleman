30ca31914029cffb49c2a0a79cd343f4999a9ab5
JaseGrable <jase.grable@gmail.com>
2023-10-03 15:35:42 -0700

    refactor code

--- league-of-average-gentlemen/src/components/home/home.component.jsx
+++ league-of-average-gentlemen/src/components/home/home.component.jsx
@@ -1,54 +1,14 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import LeagueData from '../league-data/league-data.component';
 
 const Home = () => {
-    const [leagueName, setLeagueName] = useState(null);
-  
-    useEffect(() => {
-      // Fetch the league name from the API
-      fetchLeagueName();
-  
-      async function fetchLeagueName() {
-        try {
-          const response = await fetch(`https://api.sleeper.app/v1/league/917997436273356800`);
-          const data = await response.json();
-  
-          if (data.name) {
-            setLeagueName(data.name);
-          }
-        } catch (error) {
-          console.error('Error fetching league name:', error);
-        }
-      }
-    }, []);
-  
     return (
-        <div>
-          <LeagueData leagueID="917997436273356800" leagueName={leagueName}>
-            {({ leagueData, users }) => (
-              <>
-                {leagueData && (
-                  <div>
-                    <h2>Welcome to {leagueData.name}</h2>
-                  </div>
-                )}
-                {users.length > 0 && (
-                  <div>
-                    <h3>List of Users in the League</h3>
-                    <ul>
-                      {users.map((user) => (
-                        <li key={user.user_id}>{user.display_name}</li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </>
-            )}
-          </LeagueData>
-        </div>
-      );
-                      }      
+      <div>
+        <LeagueData leagueID="917997436273356800">
+        </LeagueData>
+      </div>
+    );
+  };
   
   export default Home;
 
--- league-of-average-gentlemen/src/components/league-data/league-data.component.jsx
+++ league-of-average-gentlemen/src/components/league-data/league-data.component.jsx
@@ -1,52 +1,52 @@
 import { useState, useEffect } from 'react';
 
 const LeagueData = ({ leagueID, welcomeMessage }) => {
-    const [leagueData, setLeagueData] = useState(null);
-    const [users, setUsers] = useState([]);
-    const apiUrl = `https://api.sleeper.app/v1/league/${leagueID}`;
-  
-    useEffect(() => {
-      if (leagueID) {
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            setLeagueData(data);
-  
-            // Fetch list of users in the league
-            fetch(`${apiUrl}/users`)
-              .then((response) => response.json())
-              .then((userData) => setUsers(userData))
-              .catch((error) =>
-                console.error('Error fetching user data:', error)
-              );
-          })
-          .catch((error) => console.error('Error fetching league data:', error));
-      }
-    }, [leagueID, apiUrl]);
-  
-    return (
-    //   <div>
-    //     <h2>{welcomeMessage}</h2>
-    //     {leagueData && (
-    //       <div>
-    //         <h3>League Information</h3>
-    //         <p>League Name: {leagueData.name}</p>
-    //       </div>
-    //     )}
-        {users.length > 0 && (
-          <div>
-            <h3>List of Users in the League</h3>
-            <ul>
-              {users.map((user) => (
-                <li key={user.user_id}>{user.display_name}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default LeagueData;
+  const [leagueData, setLeagueData] = useState(null);
+  const [users, setUsers] = useState([]);
+  const apiUrl = `https://api.sleeper.app/v1/league/${leagueID}`;
+
+  useEffect(() => {
+    if (leagueID) {
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          setLeagueData(data);
+
+          
+          fetch(`${apiUrl}/users`)
+            .then((response) => response.json())
+            .then((userData) => setUsers(userData))
+            .catch((error) =>
+              console.error('Error fetching user data:', error)
+            );
+        })
+        .catch((error) => console.error('Error fetching league data:', error));
+    }
+  }, [leagueID, apiUrl]);
+
+  return (
+    <div>
+      <h2>{welcomeMessage}</h2>
+      {leagueData && (
+        <div>
+          <h3>League Information</h3>
+          <p>League Name: {leagueData.name}</p>
+        </div>
+      )}
+      {users.length > 0 && (
+        <div>
+          <h3>List of Users in the League</h3>
+          <ul>
+            {users.map((user) => (
+              <li key={user.user_id}>{user.display_name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LeagueData;
   
   
