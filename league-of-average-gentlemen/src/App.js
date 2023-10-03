4f907315091b15764fea0acbb6063fb334b3d64a
JaseGrable <jase.grable@gmail.com>
2023-10-03 13:25:51 -0700

    Routing for about and contact work, WIP home page

--- league-of-average-gentlemen/src/App.js
+++ league-of-average-gentlemen/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navigation-bar/navigation-bar.component';
+import ContactInfo from './components/contact-info/contact-info.component';
 import AboutComponent from './components/about/about.component';
+import Home from './components/home/home.component';
+import NotFound from './components/not-found/notfound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={AboutComponent} /> {/* Include the AboutComponent */}
-          <Route path="/contact" component={ContactInfo} />
-          <Route component={NotFound} /> {/* Fallback for unmatched routes */}
-        </Switch>
+        <NavigationBar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutComponent />} />
+          <Route path="/contact" element={<ContactInfo />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </Router>
   );
