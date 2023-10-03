b69bbba679fe260ed1101ffc721a944275ec1d81
JaseGrable <jase.grable@gmail.com>
2023-10-03 12:42:30 -0700

    Add about component

--- league-of-average-gentlemen/src/App.js
+++ league-of-average-gentlemen/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AboutComponent from './components/about/about.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Other components or content can go here */}
+      <AboutComponent />
+      {/* Other components or content can go here */}
     </div>
   );
 }
 
 export default App;
+
---
+++ league-of-average-gentlemen/src/components/about/about.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutComponent() {
+    return(
+        <div>
+            <h1>About The Average Gentlemen</h1>
+            <p>We are a group of friends (and Jordan) who believe like many others in America, that we are football experts. In fact, we may just be average fans. </p>
+            <p>This application was inspired by dynasty-daddy.com. I wanted to make a league website that utilizes the Sleeper API app, as well as scrapes Keep Trade Cut Values from the web.</p>
+            <p>This information will likely not make any of us better at fantasy football, but it will give me something other to blame than myself. Stupid Stats, they lie.</p>
+        </div>
+    )
+}
+
+export default AboutComponent
