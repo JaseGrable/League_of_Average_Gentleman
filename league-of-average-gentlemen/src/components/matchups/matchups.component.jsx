83fcf2dadd06e8c11557170e059cca1ba1a6cee1
JaseGrable <jase.grable@gmail.com>
2023-10-04 15:05:04 -0700

    WIP Matchups display

--- league-of-average-gentlemen/src/components/matchups/matchups.component.jsx
+++ league-of-average-gentlemen/src/components/matchups/matchups.component.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from 'react';
-import LeagueDTO from '../league-data/league-DTO';
+import React, { useState, useEffect } from 'react';
 
-const Matchups = ({ leagueID, week, leagueDTO }) => {
-  const [matchupsData, setMatchupsData] = useState(null);
+const Matchups = ({ leagueID, week }) => {
+  const [matchupsDTO, setMatchupsDTO] = useState(null);
   const apiUrl = `https://api.sleeper.app/v1/league/${leagueID}/matchups/${week}`;
 
   useEffect(() => {
-    // Fetch the matchups data when the component mounts
     fetchMatchups();
 
     async function fetchMatchups() {
@@ -14,8 +12,21 @@ const Matchups = ({ leagueID, week, leagueDTO }) => {
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        // Store the matchups data in state
-        setMatchupsData(data);
+        // Create a map to store unique matchups based on matchup_id
+        const uniqueMatchupsMap = new Map();
+
+        // Iterate through matchups and store them in the map
+        data.forEach((matchup) => {
+          if (!uniqueMatchupsMap.has(matchup.matchup_id)) {
+            uniqueMatchupsMap.set(matchup.matchup_id, matchup);
+          }
+        });
+
+        // Convert the map values back to an array
+        const uniqueMatchups = Array.from(uniqueMatchupsMap.values());
+
+        // Store the unique matchups data in state
+        setMatchupsDTO(uniqueMatchups.slice(0, 5)); // Limit to the first 5 unique matchups
       } catch (error) {
         console.error('Error fetching matchups data:', error);
       }
@@ -24,13 +35,15 @@ const Matchups = ({ leagueID, week, leagueDTO }) => {
 
   return (
     <div>
-      {matchupsData ? (
+      {matchupsDTO ? (
         <div>
           <h2>Matchups for Week {week}</h2>
           <ul>
-            {matchupsData.map((matchup) => (
+            {matchupsDTO.map((matchup) => (
               <li key={matchup.matchup_id}>
-                <p>Team {matchup.roster_id} vs. Team {matchup.roster_id}</p>
+                <p>
+                  Team {matchup.roster_id} vs. Team {matchup.opponent_roster_id}
+                </p>
               </li>
             ))}
           </ul>
@@ -43,3 +56,5 @@ const Matchups = ({ leagueID, week, leagueDTO }) => {
 };
 
 export default Matchups;
+
+
