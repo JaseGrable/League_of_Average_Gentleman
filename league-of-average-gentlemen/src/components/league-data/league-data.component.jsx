5cab522a0e845395f9b23e81ac7745d2c463aa1c
JaseGrable <jase.grable@gmail.com>
2023-10-06 14:44:47 -0700

    User name is now associated with team name

--- league-of-average-gentlemen/src/components/league-data/league-data.component.jsx
+++ league-of-average-gentlemen/src/components/league-data/league-data.component.jsx
@@ -7,12 +7,13 @@ const LeagueData = ({ leagueID, welcomeMessage }) => {
 
   useEffect(() => {
     if (leagueID) {
+      // Fetch league data
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
           setLeagueData(data);
 
-          
+          // Fetch user data
           fetch(`${apiUrl}/users`)
             .then((response) => response.json())
             .then((userData) => setUsers(userData))
@@ -23,6 +24,12 @@ const LeagueData = ({ leagueID, welcomeMessage }) => {
         .catch((error) => console.error('Error fetching league data:', error));
     }
   }, [leagueID, apiUrl]);
+
+  // Function to get team name by user_id
+  const getTeamName = (userId) => {
+    const user = users.find((user) => user.user_id === userId);
+    return user ? user.metadata.team_name : 'Unknown Team';
+  };
 
   return (
     <div>
@@ -35,10 +42,12 @@ const LeagueData = ({ leagueID, welcomeMessage }) => {
       )}
       {users.length > 0 && (
         <div>
-          <h3>List of Users in the League</h3>
+          <h3>List of User Teams in the League</h3>
           <ul>
             {users.map((user) => (
-              <li key={user.user_id}>{user.display_name}</li>
+              <li key={user.user_id}>
+                User: {user.display_name}, Team: {getTeamName(user.user_id)}
+              </li>
             ))}
           </ul>
         </div>
